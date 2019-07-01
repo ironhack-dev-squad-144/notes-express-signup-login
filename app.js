8a39b713b9fd132c65346dba4ca5b0ad481ae251
Maxence Bouret <maxence.bouret@gmail.com>
2019-07-01 18:30:27 +0100

    Done

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const logger = require("morgan");
 const path = require("path");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
+const passport = require("passport"); // For passport
+const LocalStrategy = require("passport-local").Strategy; // For passport
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
+const flash = require("connect-flash");
 
 mongoose
   .connect("mongodb://localhost/notes-express-signup-login", {
@@ -33,16 +38,18 @@ const debug = require("debug")(
 const app = express();
 
 // Middleware to enable sessions: defines a `req.session`
-app.use(session({
-  secret: "basic-auth-secret",
-  cookie: { maxAge: 60000 }, // The cookie lives 60000ms = 1 minute
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection, // Store the session in the databse
-    ttl: 24 * 60 * 60 // 1 day
-  }),
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 }, // The cookie lives 60000ms = 1 minute
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection, // Store the session in the databse
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
 // Middleware Setup
 app.use(logger("dev"));
@@ -64,6 +71,43 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// --- Passport configuration ---
+passport.serializeUser((user, cb) => {
+  console.log("serializeUser", user);
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  console.log("deserializeUser", id);
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+app.use(flash()); // Tool to use flash errors
+// Define a local strategy, that can be used to login with username and password
+passport.use(
+  // This is executed everytime we go to "POST /login" (because of the code inside auth.js)
+  // username is the value of req.body.username (and the same with password)
+  // done is a function with 3 parameters: error, user, options 
+  new LocalStrategy((username, password, done) => {
+    console.log("LocalStrategy", username, password);
+    User.findOne({ username: username })
+      .then(user => {
+        // done(null, false, {...}) => There is no error, no user, and defines a req.flash("error") to "Incorrect username"
+        if (!user) return done(null, false, { message: "Incorrect username" });
+        if (!bcrypt.compareSync(password, user.password))
+          return done(null, false, { message: "Incorrect password" });
+        // done(null, false,user) => There is no error and we can login `user`
+        return done(null, user);
+      })
+      .catch(err => done(err));
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,13 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt"); // NEW
-const bcryptSalt = 10; // NEW
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+// Shortcut for:
+// const checkLogin = require("../middlewares").checkLogin;
+const { checkLogin } = require("../middlewares");
 
 // To display the signup form
 router.get("/signup", (req, res, next) => {
@@ -33,12 +38,17 @@ router.post("/signup", (req, res, next) => {
   }
 
   // Check if the password is strong enough:
-  // - Lenght >= 7 
+  // - Lenght >= 7
   // - Has to contain a digit
   // - Has to contain a uppercase letter
-  if (password.length < 7 || !password.match(/[0-9]/) || !password.match(/[A-Z]/)) {
+  if (
+    password.length < 7 ||
+    !password.match(/[0-9]/) ||
+    !password.match(/[A-Z]/)
+  ) {
     res.render("auth/signup", {
-      errorMessage: "The password has to be at least 7 characters with some digits and uppercase letters"
+      errorMessage:
+        "The password has to be at least 7 characters with some digits and uppercase letters"
     });
     return; // To stop the function
   }
@@ -65,58 +75,96 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.get("/login", (req, res, next) => {
-  res.render("auth/login");
+  // Example on how to use `req.flash`. It can be used on different requests!!
+  // req.flash("carrot", "orange")
+  // console.log("A", req.flash("carrot")) // => [ 'orange' ]
+  // console.log("B", req.flash("carrot")) // => []
+  res.render("auth/login", {
+    errorMessage: req.flash("error")
+  });
 });
 
-router.post("/login", (req, res, next) => {
-  const theUsername = req.body.username;
-  const thePassword = req.body.password;
+// // The route without Passport
+// router.post("/login", (req, res, next) => {
+//   const theUsername = req.body.username;
+//   const thePassword = req.body.password;
 
-  if (theUsername === "" || thePassword === "") {
-    res.render("auth/login", {
-      errorMessage: "Please enter both, username and password to sign up."
-    });
-    return;
-  }
+//   if (theUsername === "" || thePassword === "") {
+//     res.render("auth/login", {
+//       errorMessage: "Please enter both, username and password to sign up."
+//     });
+//     return;
+//   }
 
-  User.findOne({ username: theUsername })
-    .then(user => {
-      if (!user) {
-        res.render("auth/login", {
-          errorMessage: "The username doesn't exist."
-        });
-        return;
-      }
-      if (bcrypt.compareSync(thePassword, user.password)) {
-        // Save the login in the session!
-        req.session.currentUser = user;
-        res.redirect("/");
-      } else {
-        res.render("auth/login", {
-          errorMessage: "Incorrect password"
-        });
-      }
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+//   User.findOne({ username: theUsername })
+//     .then(user => {
+//       if (!user) {
+//         res.render("auth/login", {
+//           errorMessage: "The username doesn't exist."
+//         });
+//         return;
+//       }
+//       if (bcrypt.compareSync(thePassword, user.password)) {
+//         // Save the login in the session!
+//         req.session.currentUser = user;
+//         res.redirect("/");
+//       } else {
+//         res.render("auth/login", {
+//           errorMessage: "Incorrect password"
+//         });
+//       }
+//     })
+//     .catch(error => {
+//       next(error);
+//     });
+// });
+
+// Login route with Passport: use the local strategy and redirect to "/" in case of success
+router.post(
+  "/login",
+  // Execute the LocalStrategy in app.js
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
 router.get("/logout", (req, res, next) => {
-  req.session.destroy((err) => {
+  req.session.destroy(err => {
     // can't access session here
     res.redirect("/login");
   });
 });
 
+// ---- Profile page ----
 // To go to the profile page, we have to be connected, otherwise we are redirected to "/login"
-router.get("/profile", (req, res, next) => {
-  if (req.session.currentUser) {
-    console.log("TCL:", req.session.currentUser);
-    res.render("profile", { user: req.session.currentUser });
-  } else {
-    res.redirect("/login");
-  }
+
+// // Solution 1
+// router.get("/profile", (req, res, next) => {
+//   // With Passport, information about the connected user is saved inside `req.user` (and not `req.session.currentUser`)
+//   if (req.user) {
+//     console.log("TCL:", req.user);
+//     res.render("profile", { user: req.user });
+//   } else {
+//     res.redirect("/login");
+//   }
+// });
+
+// // Solution 2: protect the route with a middleware from a package
+// // ensureLogin.ensureLoggedIn("/login") => middleware that redirects the user to "/login" when not connected
+// router.get(
+//   "/profile",
+//   ensureLogin.ensureLoggedIn("/login"),
+//   (req, res, next) => {
+//     res.render("profile", { user: req.user });
+//   }
+// );
+
+// Solution 3: protect the route with a custom middlleware
+router.get("/profile", checkLogin, (req, res, next) => {
+  res.render("profile", { user: req.user });
 });
 
 module.exports = router;
