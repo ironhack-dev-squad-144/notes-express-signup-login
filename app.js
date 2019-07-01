d57bca9d83f232d84d300dffddc7a38c11d9789c
Maxence Bouret <maxence.bouret@gmail.com>
2019-07-01 16:26:26 +0100

    Login done

--- app.js
+++ app.js
@@ -1,58 +1,74 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/notes-express-signup-login', {useNewUrlParser: true})
+  .connect("mongodb://localhost/notes-express-signup-login", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
+// Middleware to enable sessions: defines a `req.session`
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 }, // The cookie lives 60000ms = 1 minute
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection, // Store the session in the databse
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: false,
+  saveUninitialized: true
+}));
+
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
-
-
-app.use('/', require('./routes/index')); // Shorter
-app.use('/', require('./routes/auth')); // NEW
-
+app.use("/", require("./routes/index"));
+app.use("/", require("./routes/auth"));
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -13,19 +13,42 @@ router.get("/signup", (req, res, next) => {
 
 // To handle the signup form submission
 router.post("/signup", (req, res, next) => {
-  let { username, password } = req.body;
+  let { username, password, confirmPassword } = req.body;
   const salt = bcrypt.genSaltSync(bcryptSalt);
   const hashPass = bcrypt.hashSync(password, salt);
 
   if (username === "" || password === "") {
-    res.render("auth/signup", { errorMessage: "Please enter a username and a password" });
-    return // To stop the function
+    res.render("auth/signup", {
+      errorMessage: "Please enter a username and a password"
+    });
+    return; // To stop the function
+  }
+
+  // Check if the user typed the same password twice
+  if (password !== confirmPassword) {
+    res.render("auth/signup", {
+      errorMessage: "You've entered 2 different passwords"
+    });
+    return; // To stop the function
+  }
+
+  // Check if the password is strong enough:
+  // - Lenght >= 7 
+  // - Has to contain a digit
+  // - Has to contain a uppercase letter
+  if (password.length < 7 || !password.match(/[0-9]/) || !password.match(/[A-Z]/)) {
+    res.render("auth/signup", {
+      errorMessage: "The password has to be at least 7 characters with some digits and uppercase letters"
+    });
+    return; // To stop the function
   }
 
   User.findOne({ username: username }).then(user => {
     // If we found a user, it means the username is already taken
     if (user) {
-      res.render("auth/signup", { errorMessage: "The username is already taken" });
+      res.render("auth/signup", {
+        errorMessage: "The username is already taken"
+      });
     } else {
       User.create({
         username,
@@ -39,6 +62,61 @@ router.post("/signup", (req, res, next) => {
         });
     }
   });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ username: theUsername })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // can't access session here
+    res.redirect("/login");
+  });
+});
+
+// To go to the profile page, we have to be connected, otherwise we are redirected to "/login"
+router.get("/profile", (req, res, next) => {
+  if (req.session.currentUser) {
+    console.log("TCL:", req.session.currentUser);
+    res.render("profile", { user: req.session.currentUser });
+  } else {
+    res.redirect("/login");
+  }
 });
 
 module.exports = router;
